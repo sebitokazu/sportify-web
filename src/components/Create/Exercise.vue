90fb74c9480b937cadbd0369faaa6165ccf0eea3
sebitokazu <sebitokazu@gmail.com>
2020-10-24 20:12:29 -0300

    arreglo visual de my routines. eliminado el search de la navbar

--- src/components/Create/Exercise.vue
+++ src/components/Create/Exercise.vue
@@ -5,7 +5,12 @@
         min-width="150"
         color="background"
     >
-        <v-img class="white--text align-end" height="130px" :src="image">
+        <v-img
+            class="white--text align-end"
+            height="130px"
+            :src="image"
+            contain
+        >
             <v-card-title
                 class="primary--text py-1 pl-2"
                 style="background-color:rgba(253, 255, 242, 0.5);"
